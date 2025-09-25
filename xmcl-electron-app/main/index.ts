33e0cf84fa5fddb168c0fb8d06bde893fc92321b
BANER <86590991+BANSAFAn@users.noreply.github.com>
2025-09-25 21:29:08 +0300

    new reset github

--- xmcl-electron-app/main/index.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('graceful-fs').gracefulify(require('fs'))
-
-// eslint-disable-next-line import/first
-import ElectronLauncherApp from './ElectronLauncherApp'
-
-new ElectronLauncherApp().start()
